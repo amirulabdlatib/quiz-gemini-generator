7e760d105917dd551cb261822086b090ff12a16f
Amirul <amirulal73@gmail.com>
2025-05-16 15:55:00 +0800

    Added press enter to start quiz

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -9,7 +9,7 @@ const topic = ref("");
 <template>
   <section class="start-screen container">
     <h2>Choose a topic to start testing your knowledge</h2>
-    <input v-model="topic" type="text" placeholder="Enter your topic..." />
+    <input v-model="topic" type="text" placeholder="Enter your topic..." @keydown.enter="emit('start-quiz', topic)" />
     <button @click="emit('start-quiz', topic)" :disabled="!topic">Start Quiz</button>
   </section>
 </template>
